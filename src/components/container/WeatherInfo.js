9a808a8c8b37b32d3197b438497432f25d7e390e
Nikolai Kononov <Nikolai_Kononov@epam.com>
2020-01-28 17:32:08 +0300

    use only hooks

--- src/components/container/WeatherInfo.js
+++ src/components/container/WeatherInfo.js
@@ -1,50 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { weatherNewRequest } from '../../actions/actionsCreators';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import CityWeather from '../presentational/CityWeather';
 import { getCurrentWeatherSelector } from '../../selectors/selectors';
-import PropTypes from 'prop-types';
 
-function WeatherInfo(props) {
+
+
+export default function WeatherInfo(props) {
+
+    const weather = useSelector(state => getCurrentWeatherSelector(state));
+    const hasError = useSelector(state => state.weatherHasError);
+    const isLoading = useSelector(state => state.weatherIsLoading);
+
+    const dispatch = useDispatch();
 
     const { city } = useParams();
 
-    useEffect(() => {
-        async function fetchData() {
-            await props.fetchData(city);
-        }
-        fetchData();
-    });
+    const fetchData = useCallback(() => {
+        dispatch(weatherNewRequest(city));
+    }, [city, dispatch])
 
-    if (props.hasError) {
+    useEffect(() => {
+        fetchData()
+    }, [fetchData]);
+
+    if (hasError) {
         return <p>Sorry! there was an error loading the weather</p>
     }
 
-    if (props.isLoading) {
+    if (isLoading) {
         return <p>loading...</p>
     }
 
     return (
-        <CityWeather weather = {props.weather} city = {city} />
+        <CityWeather weather = {weather} city = {city} />
     )
     
 }
-
-const mapStateToProps = state => ({
-    weather: getCurrentWeatherSelector(state),
-    hasError: state.weatherHasError,
-    isLoading: state.weatherIsLoading
-});
-
-const mapDispatchToProps = dispatch => ({
-    fetchData: (city) => dispatch(weatherNewRequest(city))
-});
-
-WeatherInfo.propTypes = {
-    weather: PropTypes.string.isRequired,
-    hasError: PropTypes.bool.isRequired,
-    isLoading: PropTypes.bool.isRequired
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(WeatherInfo);
